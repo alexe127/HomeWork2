e2f0eb8fb608c395618c140414978db3c3a7d067
«Alex» <aser_e@mail.ru>
2022-03-22 00:28:45 +0900

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,8 @@
+﻿//Показать четные числа от 1 до N
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write(i= i + 1 );
+}
