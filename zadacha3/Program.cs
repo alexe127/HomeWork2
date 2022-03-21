010a5b2a1ca68fbb7c865e5820d111a20fa81d7f
«Alex» <aser_e@mail.ru>
2022-03-22 00:52:53 +0900

    zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else if (number <= 5)
+    Console.WriteLine("Рабочий день");
+else
+    Console.WriteLine("дня недели с таким номером не существует");
