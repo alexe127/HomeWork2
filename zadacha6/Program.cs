e300d640c4f11b01ed0a87c1c8ada27967ceda9e
«Alex» <aser_e@mail.ru>
2022-03-22 01:30:59 +0900

    zadacha6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти расстояние между точками в пространстве 2D
+Random rand = new Random ();
+int a_x = rand.Next(1,10);
+int a_y = rand.Next(1,10);
+int b_x = rand.Next(1,10);
+int b_y = rand.Next(1,10);
+
+Console.WriteLine("координаты точки А -" + a_x + ":" + a_y);
+Console.WriteLine("координаты точки B -" + b_x + ":" + b_y);
+
+double rasst = Math.Sqrt( (a_x-b_x)*(a_x-b_x)+(a_y-b_y)*(a_y-b_y));
+Console.WriteLine("расстояние между точками =" + rasst);
