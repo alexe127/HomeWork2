9dc1e1b6b19a01b5f96a809458de6242cdf2de17
«Alex» <aser_e@mail.ru>
2022-03-22 00:34:49 +0900

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать числа от -N до N
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = -num; i <= num; i++)
+{
+    Console.Write(i + " ");
+}
