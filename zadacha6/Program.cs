00e44fcb48fe7cc19fa5f75b9a46df7f0f119391
«Alex» <aser_e@mail.ru>
2022-03-22 13:22:32 +0900

    zadacha6 3d

--- zadacha6/Program.cs
+++ zadacha6/Program.cs
@@ -1,4 +1,5 @@
-﻿// Найти расстояние между точками в пространстве 2D
+﻿
+// Найти расстояние между точками в пространстве 2D
 Random rand = new Random ();
 int a_x = rand.Next(1,10);
 int a_y = rand.Next(1,10);
@@ -9,4 +10,19 @@ Console.WriteLine("координаты точки А -" + a_x + ":" + a_y);
 Console.WriteLine("координаты точки B -" + b_x + ":" + b_y);
 
 double rasst = Math.Sqrt( (a_x-b_x)*(a_x-b_x)+(a_y-b_y)*(a_y-b_y));
-Console.WriteLine("расстояние между точками =" + rasst);
+Console.WriteLine("расстояние между точками 2D =" + rasst);
+
+// 6 Найти расстояние между точками в пространстве 3D
+Random rand3d = new Random ();
+int ax = rand3d.Next(1,10);
+int ay = rand3d.Next(1,10);
+int az = rand3d.Next(1,10);
+int bx = rand3d.Next(1,10);
+int by = rand3d.Next(1,10);
+int bz = rand3d.Next(1,10);
+
+Console.WriteLine("координаты точки А -" + ax + ":" + ay + ":" + az);
+Console.WriteLine("координаты точки B -" + bx + ":" + by + ":" + bz);
+
+double rasst2= Math.Sqrt( (ax-bx)*(ax-bx)+(ay-by)*(ay-by)+(az-bz)*(az-bz));
+Console.WriteLine("расстояние между точками 3D =" + rasst2);
